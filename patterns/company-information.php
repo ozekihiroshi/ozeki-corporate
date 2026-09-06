2635c86de21e2203610ed41d6827eae897eaf0ed
Hiroshi Ozeki <ozeki.hiroshi@gmail.com>
2026-09-06 11:19:10 +0900

    Expand corporate front page patterns

---
+++ patterns/company-information.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Title: Company information
+ * Slug: ozeki-corporate/company-information
+ * Categories: about
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"backgroundColor":"surface","layout":{"type":"constrained"}} --><div class="wp-block-group alignfull has-surface-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:heading {"align":"wide"} --><h2 class="wp-block-heading alignwide"><?php echo esc_html_x('Company information', 'Company information heading', 'ozeki-corporate'); ?></h2><!-- /wp:heading --><!-- wp:table {"align":"wide"} --><figure class="wp-block-table alignwide"><table><tbody><tr><th><?php echo esc_html_x('Company', 'Company information label', 'ozeki-corporate'); ?></th><td><?php echo esc_html_x('Your organization name', 'Company information value', 'ozeki-corporate'); ?></td></tr><tr><th><?php echo esc_html_x('Location', 'Company information label', 'ozeki-corporate'); ?></th><td><?php echo esc_html_x('City, Country', 'Company information value', 'ozeki-corporate'); ?></td></tr><tr><th><?php echo esc_html_x('Business', 'Company information label', 'ozeki-corporate'); ?></th><td><?php echo esc_html_x('Your principal activities', 'Company information value', 'ozeki-corporate'); ?></td></tr></tbody></table></figure><!-- /wp:table --></div><!-- /wp:group -->
